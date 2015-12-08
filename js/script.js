cd7e4c3136ce40de954c495598a2b15c014d2e19
Matthew McLennan <MattMcLennan@github.com>
2015-12-08 04:44:52 +0000

    Fixed text typing bug --need to fix border to only show when page is loaded

--- js/script.js
+++ js/script.js
@@ -2,12 +2,15 @@ $(document).ready(function () {
   var imgString = "Hi, my name is Matthew McLennan and I make web applications work beautifully!";
   
   function write(string) {
+    var result = "";
     for (var i = 0; i < string.length; i++) {
       (function type(i) {
         setTimeout(function () {
-          // $("#img-txt").text(string[i]+" ");
-          console.log(string[i] + " ");
-        }, i * 200);
+          // Write now this function just writes each 
+          result += string[i];
+          // console.log(result);
+          $("#img-txt").text(result);
+        }, i * 125);
       })(i);
     }
   }
