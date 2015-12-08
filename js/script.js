093d7cbac459af36d35975504c3eb11e7f2921ea
Matthew McLennan <MattMcLennan@github.com>
2015-12-08 04:57:02 +0000

    Previous bug is not an actual bug and just a browser response issue

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
 $(document).ready(function () {
   var imgString = "Hi, my name is Matthew McLennan and I make web applications work beautifully!";
-  
+
   function write(string) {
     var result = "";
     for (var i = 0; i < string.length; i++) {
       (function type(i) {
         setTimeout(function () {
-          // Write now this function just writes each 
           result += string[i];
-          // console.log(result);
           $("#img-txt").text(result);
         }, i * 125);
       })(i);
