f83aa9abde31c37fcefe5f367ddbad137693e2d0
Matthew McLennan <MattMcLennan@github.com>
2015-12-08 04:00:51 +0000

    Added js typing on screen load with closure and set timeout function in console log

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 $(document).ready(function () {
-  console.log("hello");
+  var imgString = "Hi, my name is Matthew McLennan and I make web applications work beautifully!";
+  
+  function write(string) {
+    for (var i = 0; i < string.length; i++) {
+      (function type(i) {
+        setTimeout(function () {
+          console.log(string[i]+" ");
+          var time = new Date();
+          console.log(time);
+        }, i * 200);
+      })(i);
+    }
+  }
+
+  write(imgString);
+
 })
