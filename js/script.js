6d471cfc622ee53b5d4c6dcc319a153ea9fa9b3c
Matthew McLennan <MattMcLennan@github.com>
2015-12-15 04:58:18 +0000

    MAde the write function on main image outside document ready function for ease of reading purposes

--- js/script.js
+++ js/script.js
@@ -1,18 +1,5 @@
 $(document).ready(function () {
   var imgString = "and I make web applications work beautifully!";
-
-  function write(string) {
-    var result = "Hi, my name is Matthew McLennan ";
-    for (var i = 0; i < string.length; i++) {
-      (function type(i) {
-        setTimeout(function () {
-          result += string[i];
-          $("#img-txt").text(result);
-        }, i * 125);
-      })(i);
-    }
-  }
-
   write(imgString);
 
   $('td img').mouseover(function() {
@@ -20,3 +7,15 @@ $(document).ready(function () {
   });
 
 })
+  
+function write(string) {
+  var result = "Hi, my name is Matthew McLennan ";
+  for (var i = 0; i < string.length; i++) {
+    (function type(i) {
+      setTimeout(function () {
+        result += string[i];
+        $("#img-txt").text(result);
+      }, i * 125);
+    })(i);
+  }
+}
