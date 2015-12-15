7d9a603eca4df5869f2c24c91c57e7a363d4b2f2
Matthew McLennan <MattMcLennan@github.com>
2015-12-15 06:19:18 +0000

    Added larger image when user hovers over portfolio small image

--- js/script.js
+++ js/script.js
@@ -3,11 +3,19 @@ $(document).ready(function () {
   write(imgString);
 
   $('td img').mouseover(function() {
+    if (('#folio').length > 0) {
+      $('#folio').remove();
+    }
+
+    $img = $(this).attr('src');
+    $('.portfolio-large').append("<img id='folio' src='"+$img+"'>");
     debugger
   });
 
 })
-  
+
+// This function writes the cool text on the main image
+// seem like someone is typing it live 
 function write(string) {
   var result = "Hi, my name is Matthew McLennan ";
   for (var i = 0; i < string.length; i++) {
